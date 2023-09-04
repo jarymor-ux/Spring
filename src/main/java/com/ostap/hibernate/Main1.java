1a3f8e8bf089af6460a4394fe39043eedf19f255
Ostap <jarymorfifa@gmail.com>
2023-09-04 03:50:08 +0300

    jjjj

--- src/main/java/com/ostap/hibernate/Main1.java
+++ src/main/java/com/ostap/hibernate/Main1.java
@@ -1,7 +1,27 @@
 package com.ostap.hibernate;
 
+
+import com.ostap.hibernate.Entity.Employee;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main1 {
     public static void main(String[] args) {
-        
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try{Session session = factory.getCurrentSession();
+        Employee employee = new Employee("Ostap", "Evdokimov", "IT", 1500);
+        session.beginTransaction();
+        session.save(employee);
+        session.getTransaction().commit();
+        session.close();
+        }finally{
+            factory.close();
+        }
     }
 }
